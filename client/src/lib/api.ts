6a38382b12e942480b07208f8505911b86aaabbc
Daniel Hernandez <danielmark252525@GMAIL.COM>
2026-01-18 21:33:08 -0500

    Fix client class syntax and exclude tests from server tsc check

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -47,12 +47,12 @@ export interface RunAnalysisInput {
 }
 
 export class ApiError extends Error {
-  constructor(
-    public status: number,
-    message: string
-  ) {
+  status: number;
+
+  constructor(status: number, message: string) {
     super(message);
     this.name = 'ApiError';
+    this.status = status;
   }
 }
 
